628ed4eec09f2b312e8fbe62d062c21b03c107a5
Dup4 <lyuzhi.pan@gmail.com>
2021-08-07 16:49:29 +0800

    chore: rename shell_folder -> top_dir

--- top_dir.sh
+++ top_dir.sh
@@ -3,7 +3,7 @@
 TOP_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
 # if the current script path is 
-# ~/Shell-Script-Snippet/shell_folder.sh
+# ~/Shell-Script-Snippet/top_dir.sh
 # the value of ${TOP_DIR} is 
 # ~/Shell-Script-Snippet 
 echo "${TOP_DIR}"
