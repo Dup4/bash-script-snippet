a632f881165d730d0cdbd9c69111365ecabdea09
Dup4 <lyuzhi.pan@gmail.com>
2021-08-07 16:55:30 +0800

    chore: add test code

--- top_dir.sh
+++ top_dir.sh
@@ -2,6 +2,8 @@
 
 TOP_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
+# the following code is the test code
+
 # if the current script path is 
 # ~/Shell-Script-Snippet/top_dir.sh
 # the value of ${TOP_DIR} is 
