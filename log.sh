8c37e1928d3f674801ac999ea05161bde31a1de5
Dup4 <lyuzhi.pan@gmail.com>
2021-08-07 16:58:51 +0800

    feat: log

---
+++ log.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+function get_now_time() {
+	# https://unix.stackexchange.com/questions/120484/what-is-a-standard-command-for-printing-a-date-in-rfc-3339-format
+	NOW_TIME=$(date --rfc-3339=ns | sed 's/ /T/; s/\(\....\).*\([+-]\)/\1\2/g')
+	echo "${NOW_TIME}"
+}
+
+function INFO() {
+	echo -e "\033[0;32m$(get_now_time) [INFO]: $*\033[0m"
+}
+
+function ERROR() {
+	echo -e "\033[0;31m$(get_now_time) [ERROR]: $*\033[0m"
+}
+
+# the following code is the test code
+
+INFO "this is a info log."
+ERROR "this is a error log."
