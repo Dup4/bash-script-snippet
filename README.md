b0cfeb800d80dbe8b0b2ae5264e95c579c9b910b
Dup4 <lyuzhi.pan@gmail.com>
2022-01-16 13:12:59 +0800

    docs: add logo

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Shell-Script-Snippet
-Commonly used shell script snippet
+<img align="right" width="96px" src="./assets/1200px-Bash_Logo_Colored.svg.png">
+
+# Bash Script Snippet
+Commonly used bash script snippet
--- top_dir.sh
+++ top_dir.sh
@@ -4,8 +4,8 @@ TOP_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
 # the following code is the test code
 
-# if the current script path is 
+# if the current script path is
 # ~/Shell-Script-Snippet/top_dir.sh
-# the value of ${TOP_DIR} is 
-# ~/Shell-Script-Snippet 
+# the value of ${TOP_DIR} is
+# ~/Shell-Script-Snippet
 echo "${TOP_DIR}"
