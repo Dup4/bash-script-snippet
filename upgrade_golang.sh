85c2a1d2222ecfcc0715b48c5d90316a5a4e8766
Dup4 <lyuzhi.pan@gmail.com>
2022-03-17 16:07:06 +0800

    feat: upgrade_golang.sh

---
+++ upgrade_golang.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+GO_INSTALL_PATH="/usr/local/go"
+GO_LATEST_FILENAME="go1.18.linux-amd64.tar.gz"
+GO_LATEST_DOWNLOAD_URL="https://go.dev/dl/${GO_LATEST_FILENAME}"
+GO_LATEST_DOWNLOAD_PATH="/tmp/${GO_LATEST_FILENAME}"
+
+wget --tries=3 "${GO_LATEST_DOWNLOAD_URL}" -O "${GO_LATEST_DOWNLOAD_PATH}"
+
+if [[ -s "${GO_LATEST_DOWNLOAD_PATH}" ]]; then
+  if [[ -d "${GO_INSTALL_PATH}" ]]; then
+      sudo rm -rf "${GO_INSTALL_PATH}"
+  fi
+
+  sudo tar -C /usr/local -xzf "${GO_LATEST_DOWNLOAD_PATH}"
+  rm -rf "${GO_LATEST_DOWNLOAD_PATH}"
+
+  go version
+fi
