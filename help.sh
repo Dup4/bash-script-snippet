30503aa9f75bab57e383e5400443a87ce80a5383
Dup4 <lyuzhi.pan@gmail.com>
2022-05-02 10:35:31 +0800

    feat: help script

---
+++ help.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+cat <<EOF
+delete_files_with_specified_suffix
+list_proc_used_memory_score
+deploy_gh_pages
+modify_file_extensions
+EOF
--- list_proc_used_memory_score.sh
+++ list_proc_used_memory_score.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#! /bin/bash
 
 # shellcheck disable=SC2044
 # shellcheck disable=SC2002
